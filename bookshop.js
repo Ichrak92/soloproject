b9233a49544df9c2b9e705ccf247f5a124fddff5
Ichrak92 <56369109+Ichrak92@users.noreply.github.com>
2021-01-10 15:38:27 +0100

    mysoloproject

--- bookshop.js
+++ bookshop.js
@@ -1,15 +1,15 @@
 //SlideShow
-var myIndex = 0;
-carousel();
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("autoSlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000);
-}
+//var myIndex = 0;
+//function carousel() {
+  //var i;
+  //var x = document.getElementsByClassName("autoSlides");
+  //for (i = 0; i < x.length; i++) {
+   // x[i].style.display = "none";  
+  //}
+  //myIndex++;
+  //if (myIndex > x.length) {myIndex = 1}    
+  //x[myIndex-1].style.display = "block";  
+  //setTimeout(carousel, 2000);
+//}
+
 
